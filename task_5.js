d088703b76f353f093d1cbc57664d2ae34d28864
Igor Bereza <igor.bereza@gmail.com>
2021-08-14 10:51:28 +0300

    dz_2-5

---
+++ task_5.js
@@ -0,0 +1,25 @@
+/*
+Реализовать основные 4 арифметические операции в виде функций с двумя параметрами.
+Обязательно использовать оператор return.
+*/
+
+function sum(a, b) {
+    return a + b;
+}
+
+function sub(a, b) {
+    return a - b;
+}
+
+function mul(a, b) {
+    return a * b;
+}
+
+function dev(a, b) {
+    return a / b;
+}
+
+console.log(sum(2, -5));
+console.log(sub(2, -5));
+console.log(mul(2, -5));
+console.log(dev(2, -5));
